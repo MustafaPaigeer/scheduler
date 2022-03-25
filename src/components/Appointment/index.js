315cbf2421fad864f046e79f2182eb43129edd44
Mustafa Paigeer <mustafapaigeer@live.com>
2022-03-25 00:45:54 -0700

    Add Appointment Empty and Appointment Booked in appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empy";
 
 
 export default function Appointment(props) {
-    const appointment = props.time ? `Appoitment at ${props.time}` : "No Appoitments";
+    const appointment = props.time ?  `${props.time}` : "no appoitment";
+    const interview = props.interview ? <Show />: <Empty />;
   
       return (
         <article className="appointment">
             {appointment}
+            {Header}
+            {interview}
         </article>
     )
   };
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -171,7 +171,7 @@ storiesOf("Appointment", module)
 
    .add("Create", () => 
    <Form
-   interviewer={interviewer}
+   interviewer={4}
    onSave={action("onSave")}
    onCancel={action("onCancel")} />)
 
@@ -182,4 +182,22 @@ storiesOf("Appointment", module)
    interviewers={interviewers}
    onSave={action("onSave")}
    onCancel={action("onCancel")}
-   />);
+   />)
+
+   .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ));
